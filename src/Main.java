090043f88572ea92fa46d19456c56f8e0662dc3a
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-08 10:35:22 +0100

    Erklärung

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-public class Main {
+public class Main // erste Klasse die automatisch beim öffnen eines neuen Projekts vorhanden war
+{
 
-    public static void main(String[] args)
+    public static void main(String[] args)// ist die Deklarierung, die uns später die Werte wieder gibt
     {
-        System.out.println("Hello World!");
-        System.out.println("Sabrina");
-    }
+        System.out.println("Hello World!"); // zeigt uns an was er später ausgeben soll
+        System.out.println("Sabrina"); // zeigt uns an was er später ausgeben soll
 }
--- src/Package2/NeueKlasse.java
+++ src/Package2/NeueKlasse.java
@@ -1,9 +1,9 @@
-package Package2;
+package Package2; //wurde von uns erstellt, nachdem wir bei SRC rechte Maustaste gedrückt haben und New - Java Class ausgewählt haben
 
-public class NeueKlasse
+public class NeueKlasse // der Name der Klasse wurde von uns selbst gewählt
 {
-    public static void main(String[] args)
+    public static void main(String[] args) // da wir einen Text ausgeben mussten haben wir die Main-String Methode gewählt
     {
-        System.out.println("FH Kufstein Tirol");
+        System.out.println("FH Kufstein Tirol"); // der Text der ausgegeben werden soll, sout danach Tabstopp und dann in den Klammern und den Anführungszeichen den Text eingeben
     }
 }
