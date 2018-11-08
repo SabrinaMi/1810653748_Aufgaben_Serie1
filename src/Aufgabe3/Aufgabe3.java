ca42249dbae574584ef04671d9c2216156864609
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-08 11:23:04 +0100

    Aufgabe 5

--- src/Aufgabe3/Aufgabe3.java
+++ src/Aufgabe3/Aufgabe3.java
@@ -2,18 +2,15 @@ package Aufgabe3;
 
 import javax.naming.Name;
 
-public class Aufgabe3
-{
-    public static void main (String[] args)
-    {
+public class Aufgabe3 {
+    public static void main(String[] args) {
         String name = "Sabrina";
         System.out.println(name);
         int alter = 25;
         System.out.println((alter));
         final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol"; //rechte Maustaste - Refactor - Rename und neu eingeben
         // final bedeutet das die ganze Zeile Final geändert wurde, es benötigt als erstes Wort inder Zeile final und dann muss der Variablenname in Grossbuchstaben geschrieben werden
-        
-        
+        final String hallo;
     }
 
 }
