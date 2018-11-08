d84efa247c9c2b71a175c033bb4d9df5354a8500
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-08 11:11:40 +0100

    Aufgabe 4

--- src/Aufgabe3/Aufgabe3.java
+++ src/Aufgabe3/Aufgabe3.java
@@ -10,6 +10,10 @@ public class Aufgabe3
         System.out.println(name);
         int alter = 25;
         System.out.println((alter));
+        final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol"; //rechte Maustaste - Refactor - Rename und neu eingeben
+        // final bedeutet das die ganze Zeile Final geändert wurde, es benötigt als erstes Wort inder Zeile final und dann muss der Variablenname in Grossbuchstaben geschrieben werden
+        
+        
     }
 
 }
